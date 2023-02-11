044a92626d14b3f582fbc97b56be9d79691c7dde
Grace229 <itiunggrace@gmail.com>
2023-02-11 16:59:18 +0100

    add get order route

--- controllers/order/getOrder.js
+++ controllers/order/getOrder.js
@@ -3,7 +3,7 @@ const { Order } = require('../../models/Order');
 const getSingleOrder = async(req, res) => {
     try {
         let  {orderId}  =  req.params;
-        let order = await Order.findOne({ _id : orderId})
+        let order = await Order.findOne({ _id : orderId}).populate('orderOwner').populate('products.product').populate('products.vendorId').populate('address');
         if (!order) return res.status(500).json({ success: false, msg: 'No order found' });
 
         return res.status(200).json({
